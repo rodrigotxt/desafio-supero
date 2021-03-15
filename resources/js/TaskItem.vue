bcc23caa15e72f5918a9e297c51f6d001509b96e
Rodrigo Martins <rodrigotxt@gmail.com>
2021-03-14 23:07:03 -0300

    Implementacao de metodos para atualizacao da Tarefa

--- resources/js/TaskItem.vue
+++ resources/js/TaskItem.vue
@@ -1,27 +1,35 @@
 <template>
-	  <li @blur="onblur" class="list-group-item d-flex justify-content-between align-items-center">
-	  	<div class="form-check form-check-inline">
-	  	  <input @input="isCompleted" class="form-check-input" type="checkbox" value="task.status" :checked="task.status == 'concluído'">
-	  	  <span @click="isCompleted" :contentEditable="active" :class="{closed: task.status == 'concluído'}">{{task.title}}</span>
-	  	</div>
-		 <span class="badge badge-primary badge-pill">{{task.status}}</span>
+	<li @blur="onblur" class="list-group-item d-flex justify-content-between align-items-center">
+		<div class="form-check form-check-inline">
+			<input @input="changeStatus" class="form-check-input" type="checkbox" value="task.status" :checked="task.status == 'concluído'">
+			<span class="title" @blur="onblur"  @click="active = true" :contentEditable="active" :class="{closed: task.status == 'concluído', 'edit': (!task.id || active)}">{{task.title}}</span>
+		</div>
+		<span class="badge badge-pill" :class="{'badge-primary': task.status == 'em aberto', 'badge-secondary': task.status == 'concluído'}">{{task.status}}</span>
+		<span class="actions">
+			<button class="btn btn-sm btn-success" v-if="!task.id || (active && !isComplete)" title="Atualizar">
+				<span class="small material-icons">done</span>
+			</button>
+			<button v-if="task.id" title="Excluir" class="btn btn-sm btn-danger btn-rounded"><span class="small material-icons">delete_forever</span></button>
+		</span>
 	</li>
 </template>
 
 <script>
 	export default{
 		name: 'TarefaItem',
-		props: ['task'],
+		props: ['task', 'index'],
 		data: function(){
 			return {
 				active: false,
 			}
 		},
 		methods: {
-			isCompleted(event){
-				// let checked = event.target.checked;
+			update(){
+				this.$emit('update', task);
+			},
+			// metodo para verificar se a tarefa está concluída
+			changeStatus(event){
 				let status = this.task.status;
-				// let task = this.task;
 				if(status == 'em aberto'){
 					this.$emit('update:status', 'concluído');
 				}else{
@@ -29,12 +37,19 @@
 				}
 			},
 			onblur(event){
-				// let task = event.task;
-				// alert('perdeu foco');
+				let text = event.target.innerText;
+				this.task.title = text;
+				this.active = false;
+				this.$emit('update:title', this.task);
+			}
+		},
+		computed: {
+			isComplete(){
+				return this.task.status == 'concluído' ? true : false;
 			}
 		},
 		mounted(){
-			if(this.task.title == 'Adicionar'){
+			if(this.task.title == ' '){
 				this.active = true;
 			}
 		}
@@ -42,11 +57,23 @@
 </script>
 
 <style lang="scss">
+
+	li input[type='checkbox']{
+		cursor: pointer;
+	}
+	span.edit{
+		cursor: text;
+		border-bottom: 1px solid #CCC;
+	}
+	.title{
+		margin-left: .5em;
+		width: 150px;
+	}
 	.closed{
+		color:gray;
 		text-decoration: line-through;
 	}
 	.form-check-input{
 		transform: scale(1.3);
 	}
-	/*@import '../css/bootstrap.css';*/
 </style>
