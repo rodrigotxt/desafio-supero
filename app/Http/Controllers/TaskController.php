64cd615ec864371484f0c4e31cbd9a3bdbd2ac50
Rodrigo Martins <rodrigotxt@gmail.com>
2021-03-14 23:01:46 -0300

    Tabela e recursos para Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
@@ -12,6 +14,18 @@ class TaskController extends Controller
     public function __construct()
     {
         //
+    }
+
+    public function update(Request $request){
+        if($request->id){
+            $task = Task::find($request->id);
+        }else{
+            $task = New Task;
+            $task->title = $request->title;
+            $task->status = 'em aberto';
+            $task->save();
+            return $task;
+        }
     }
 
     //
