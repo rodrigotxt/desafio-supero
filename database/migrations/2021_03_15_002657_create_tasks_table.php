50b792c5e5fe70eb9ae0b935c92fbdd31e1f4ef4
Rodrigo Martins <rodrigotxt@gmail.com>
2021-03-15 00:33:32 -0300

    Migration para tabela Tasks

--- database/migrations/2021_03_15_002657_create_tasks_table.php
+++ database/migrations/2021_03_15_002657_create_tasks_table.php
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('status');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
