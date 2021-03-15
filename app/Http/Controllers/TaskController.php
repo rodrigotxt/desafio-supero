32ab6be470175eaa0cc42b2265e69a683fe25bca
Rodrigo Martins <rodrigotxt@gmail.com>
2021-03-15 05:25:28 -0300

    Implementacao funcoes no TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Task;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
@@ -16,17 +16,32 @@ class TaskController extends Controller
         //
     }
 
+    public function all(){
+        return Task::all();
+    }
+
     public function update(Request $request){
-        if($request->id){
-            $task = Task::find($request->id);
-        }else{
+        if(!$request->id){
             $task = New Task;
             $task->title = $request->title;
             $task->status = 'em aberto';
             $task->save();
             return $task;
+        }else{
+            $task = Task::find($request->id);
+            $task->title = $request->title;
+            $task->status = $request->status;            
+            $task->save();
+            return $task;
         }
     }
 
-    //
+    public function remove($id){
+        try {         
+            $task = Task::find($id);
+            return $task->delete();
+        } catch (Exception $e) {
+            return response('Ocorreu um erro.', 400);
+        }
+    }
 }
