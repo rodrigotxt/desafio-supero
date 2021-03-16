e4a301f6abc2f1aff4b964a8f871258b406ee677
Rodrigo Martins <rodrigotxt@gmail.com>
2021-03-16 04:15:36 -0300

    Config para Cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,12 +14,10 @@ class CorsMiddleware
     {
         $headers = [
         'Access-Control-Allow-Origin'      => '*',
-        'Access-Control-Allow-Methods'     => 'POST, GET, 
-        OPTIONS,PUT,DELETE',
+        'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS,PUT,DELETE',
         'Access-Control-Allow-Credentials' => 'true',
         'Access-Control-Max-Age'           => '86400',
-        'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, 
-        X-Requested-With'
+        'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
         if ($request->isMethod('OPTIONS'))
